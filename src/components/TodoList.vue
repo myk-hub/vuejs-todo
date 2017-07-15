8e386c52ed13bc7a8e232473a8e74a31bce20189
usernamen7 <n7username7@gmail.com>
2017-07-15 15:41:47 +0300

    Semantic, add TodoList, Todo components

---
+++ src/components/TodoList.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <p>Completed tasks: {{todos.filter(todo => {return todo.done === true}).length}}</p>
+    <p>Pending: {{todos.filter(todo => {return todo.done === false}).length}}</p>
+    <todo v-for="todo in todos" v-bind:todo="todo"></todo>
+  </div>
+</template>
+
+<script type="text/javascript">
+  import Todo from './Todo'
+
+  export default {
+    props: ['todos'],
+    components: {
+      Todo
+    }
+  }
+</script>
+
+<style>
+</style>
